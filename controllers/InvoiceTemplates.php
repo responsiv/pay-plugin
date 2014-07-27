0c4866c4c0aeb7e7c6b79e6bc897c56b9b44e705
Sam Georges <sam@daftspunk.com>
2014-07-27 14:10:03 +1000

    Patch for latest build

--- controllers/InvoiceTemplates.php
+++ controllers/InvoiceTemplates.php
@@ -4,6 +4,7 @@ use Backend;
 use Redirect;
 use BackendMenu;
 use Backend\Classes\Controller;
+use System\Classes\SettingsManager;
 
 /**
  * InvoicesTemplates Back-end Controller
@@ -21,6 +22,7 @@ class InvoiceTemplates extends Controller
         parent::__construct();
 
         BackendMenu::setContext('October.System', 'system', 'settings');
+        SettingsManager::setContext('Responsiv.Pay', 'invoices');
     }
 
     public function index()
