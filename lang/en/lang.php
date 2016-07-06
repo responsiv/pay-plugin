6e9b9ba0c54f9633f9c8d308d965fe7f51574618
reg2005 <cmp08@ya.ru>
2016-07-06 23:06:16 +0300

    Added lang string

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,7 +1,23 @@
 <?php
 
 return [
+    'name' => 'Pay',
+    'description' => 'Invoicing and Accounting',
     'add_payment_method' => 'Add Payment Method',
+    'menu' => [
+        'payments' => 'Payments',
+        'invoices' => 'Invoices',
+        'tax' => 'Tax tables',
+        'gateways' => 'Gateways',
+    ],
+    'settings' => [
+        'name' => 'Payment settings',
+        'description' => 'Manage payment configuration'
+    ],
+    'invoice_template' => [
+        'name' => 'Invoice template',
+        'description' => 'Customize the template used for invoices.',
+    ],
     'invoice' => [
         'change_status_title' => 'Change Invoice Status',
         'current_status_name' => 'Current status: :name',
