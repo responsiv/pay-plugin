3baf7902a014cf1a83eaed91ebe41a0147bb9591
Samuel Georges <sam@daftspunk.com>
2024-03-25 12:00:04 +1100

    Docs

--- contracts/Invoice.php
+++ contracts/Invoice.php
@@ -1,39 +1,39 @@
 <?php namespace Responsiv\Pay\Contracts;
 
 /**
- * This contract represents an Payment Object that will be used
+ * Invoice contract represents an Payment Object that will be used
  * to record instances where payment attempts are made.
  */
 interface Invoice
 {
     /**
-     * Returns a unique identifier for this payment object.
+     * getUniqueId returns a unique identifier for this payment object.
      * @return string
      */
     public function getUniqueId();
 
     /**
-     * Locates a Payment object by its unique identifier.
+     * findByUniqueId locates a Payment object by its unique identifier.
      * @param  string $hash
      * @return self
      */
     public function findByUniqueId($id = null);
 
     /**
-     * Returns a hashed identifier for this payment object.
+     * getUniqueHash returns a hashed identifier for this payment object.
      * @return string
      */
     public function getUniqueHash();
 
     /**
-     * Locates a Payment object by its hashed identifier.
+     * findByUniqueHash locates a Payment object by its hashed identifier.
      * @param  string $hash
      * @return self
      */
     public function findByUniqueHash($hash = null);
 
     /**
-     * Should return a URL for viewing this invoice.
+     * getReceiptUrl should return a URL for viewing this invoice.
      * @return string
      */
     public function getReceiptUrl();
@@ -57,7 +57,7 @@ interface Invoice
     public function getCustomerDetails();
 
     /**
-     * Returns an array of line items. Each line item is an array and must contain (at minimum):
+     * getLineItemDetails returns an array of line items. Each line item is an array and must contain (at minimum):
      *
      * - description: Description of item
      * - quantity: Amount of items
@@ -69,7 +69,7 @@ interface Invoice
     public function getLineItemDetails();
 
     /**
-     * Returns an array of totals. Must contain:
+     * getTotalDetails returns an array of totals. Must contain:
      *
      * - total: Total cost amount
      * - subtotal: Total cost without tax
@@ -81,7 +81,7 @@ interface Invoice
     public function getTotalDetails();
 
     /**
-     * Check if the payment has been processed on this Payment.
+     * isPaymentProcessed checks if the payment has been processed on this Payment.
      * It should be cached to allow multiple calls.
      * @param  boolean $force Ignore the cache.
      * @return boolean        Returns true if the payment is processed.
@@ -89,21 +89,21 @@ interface Invoice
     public function isPaymentProcessed($force = false);
 
     /**
-     * Flags this Payment as having payment processed. This method should
+     * markAsPaymentProcessed flags this Payment as having payment processed. This method should
      * only be callable once.
      * @return boolean Returns false if method has already been called.
      */
     public function markAsPaymentProcessed();
 
     /**
-     * Returns a configured payment method object used for this Payment object.
+     * getPaymentMethod returns a configured payment method object used for this Payment object.
      * This should return a configured Model that implements the Responsiv\Pay\Classes\GatewayBase behavior.
      * @return Model
      */
     public function getPaymentMethod();
 
     /**
-     * Adds a log record to the invoice payment attempts log.
+     * logPaymentAttempt adds a log record to the invoice payment attempts log.
      * @param string $message           Log message.
      * @param bool   $isSuccess         Indicates that the attempt was successful.
      * @param array  $requestArray      An array containing data posted to the payment gateway.
@@ -120,7 +120,7 @@ interface Invoice
     );
 
     /**
-     * Updates the invoice status to the supplied code.
+     * updateInvoiceStatus updates the invoice status to the supplied code.
      * @param  string $statusCode
      * @return void
      */
