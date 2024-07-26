b7083651e9f5b64c0907c074fea1c660edde52ae
October CMS <hello@octobercms.com>
2024-07-26 12:53:52 +1000

    Minor

--- components/Invoice.php
+++ components/Invoice.php
@@ -1,12 +1,13 @@
 <?php namespace Responsiv\Pay\Components;
 
 use Auth;
-use Request;
-use Redirect;
 use Cms\Classes\Page;
 use Cms\Classes\ComponentBase;
 use Responsiv\Pay\Models\Invoice as InvoiceModel;
 
+/**
+ * Invoice
+ */
 class Invoice extends ComponentBase
 {
     /**
@@ -14,43 +15,55 @@ class Invoice extends ComponentBase
      */
     protected $invoice;
 
+    /**
+     * componentDetails
+     */
     public function componentDetails()
     {
         return [
-            'name'        => 'Invoice Component',
+            'name' => 'Invoice Component',
             'description' => 'Allow an owner to view their invoice by its identifier'
         ];
     }
 
+    /**
+     * defineProperties
+     */
     public function defineProperties()
     {
         return [
             'id' => [
-                'title'       => 'Invoice ID',
+                'title' => 'Invoice ID',
                 'description' => 'The URL route parameter used for looking up the invoice by its identifier.',
-                'default'     => '{{ :id }}',
-                'type'        => 'string'
+                'default' => '{{ :id }}',
+                'type' => 'string'
             ],
             'payPage' => [
-                'title'       => 'Payment page',
+                'title' => 'Payment page',
                 'description' => 'Name of the payment page file for the "Pay this invoice" links.',
-                'type'        => 'dropdown'
+                'type' => 'dropdown'
             ],
             'isPrimary' => [
-                'title'       => 'Primary page',
+                'title' => 'Primary page',
                 'description' => 'Link to this page when sending mail notifications.',
-                'type'        => 'checkbox',
-                'default'     => true,
+                'type' => 'checkbox',
+                'default' => true,
                 'showExternalParam' => false
             ],
         ];
     }
 
+    /**
+     * getPropertyOptions
+     */
     public function getPropertyOptions($property)
     {
         return Page::sortBy('baseFileName')->lists('baseFileName', 'baseFileName');
     }
 
+    /**
+     * onRun
+     */
     public function onRun()
     {
         $this->page['payPage'] = $this->payPage();
@@ -63,6 +76,9 @@ class Invoice extends ComponentBase
         }
     }
 
+    /**
+     * invoice
+     */
     protected function invoice()
     {
         if ($this->invoice !== null) {
@@ -75,10 +91,8 @@ class Invoice extends ComponentBase
 
         $invoice =  InvoiceModel::where('id', $id)->first();
 
-        /*
-         * Only users can view their own invoices
-         */
-        $user = Auth::getUser();
+        // Only users can view their own invoices
+        $user = Auth::user();
         if (!$user) {
             $invoice = null;
         }
@@ -94,6 +108,9 @@ class Invoice extends ComponentBase
         return $this->invoice = $invoice;
     }
 
+    /**
+     * payPage
+     */
     protected function payPage()
     {
         return $this->property('payPage');
