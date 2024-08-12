62364dffb42d2f2f19f5817192e4191067bcadc7
Samuel Georges <sam@daftspunk.com>
2024-08-13 08:57:39 +1000

    Update Stripe instructions to latest

--- paymenttypes/stripepayment/_setup_help.php
+++ paymenttypes/stripepayment/_setup_help.php
@@ -5,7 +5,12 @@
     </div>
     <div class="content">
         <ol>
-            <li>Log in to your Stripe account</li>
+            <li>Log in to your <a href="https://www.stripe.com/" target="_blank">Stripe account</a></li>
+            <li>Click the <strong>Developers</strong> link in the menu</li>
+            <li>Toggle the <strong>Live / Sandbox</strong> option depending on your requirements</li>
+            <li>Select <strong>API keys</strong> link in the submenu</li>
+            <li>Under <strong>Standard Keys</strong> click <strong>Reveal Live Key Token</strong></li>
+            <li>Copy the <strong>Secret Key</strong> and paste it in the Configuration tab</li>
         </ol>
     </div>
 </div>
