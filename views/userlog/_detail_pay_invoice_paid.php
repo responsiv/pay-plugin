b62b0142032b216d17836313fe66bb680f9d0c9a
Samuel Georges <sam@daftspunk.com>
2026-03-29 11:51:08 +1100

    Include invoice # in activity log

--- views/userlog/_detail_pay_invoice_paid.php
+++ views/userlog/_detail_pay_invoice_paid.php
@@ -1,4 +1,9 @@
-<?= __(":name paid an invoice for :total", [
+<?php
+$invoiceUrl = Backend::url("responsiv/pay/invoices/preview/{$record->invoice_id}");
+$invoiceLink = '<a href="'.e($invoiceUrl).'">'.__("Invoice #:id", ['id' => $record->invoice_id]).'</a>';
+?>
+<?= __(":name paid :invoice for :total", [
     'name' => $record->actor_user_name_linked,
+    'invoice' => $invoiceLink,
     'total' => Currency::format($record->invoice_total ?: 0),
 ]) ?>
